74d9a65dc797547ff56bd328b772481d00ad703f
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-06-01 23:59:32 +0530

    create-react-app init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chess Training Games
+
+```
+As a big fan of Chess
+and want to train, learn chess more
+
+So as a programmer thinking what to do
+
+So started creating a project on Chess Training
+```
+
+# Links
+
+-   [@withrvr on chess.com](https://www.chess.com/member/withrvr)
+-   [chessboard js api](https://chessboardjs.com/)
+
+### Currently Under Development
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="container my-5 text-center">
+				<h3 className="mb-5">Chess Training Games</h3>
+				<div
+					style={{
+						textAlign: "justify",
+					}}
+				>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Ullam vel alias eos molestiae corporis sint molestias
+					similique at autem. Laudantium modi totam deserunt autem
+					eaque quibusdam at ab impedit eius beatae aperiam blanditiis
+					nemo, quidem molestias voluptates excepturi quisquam aut
+					tenetur sit odio natus provident. Reprehenderit
+					exercitationem quos aliquid, et sunt quas dolores esse animi
+					sint neque labore fugit, deleniti accusamus qui iusto atque
+					suscipit tenetur dignissimos laborum, corrupti perspiciatis
+					dolor voluptas optio nulla. Exercitationem, voluptate eos
+					iusto sed eius minus tempore iure impedit earum consequuntur
+					eum sapiente, temporibus ea perspiciatis repellat aperiam
+					cumque a quo deleniti ipsam doloremque? Asperiores quidem
+					fugit maiores hic veniam perspiciatis quis modi rerum earum
+					magnam libero ullam cupiditate omnis recusandae, ex expedita
+					minus sunt esse consequatur doloribus neque iure dolorem
+					unde? Vitae explicabo placeat, sequi ducimus, quos
+					laboriosam inventore voluptatem laudantium itaque quibusdam
+					aut, aliquid nihil nesciunt quaerat vel tempore quae ipsam
+					natus. Architecto iure, laboriosam accusantium libero facere
+					fuga quo sit nam in molestiae repellat, cum optio debitis
+					accusamus dolorum magnam alias dicta iste atque odit eum
+					aliquid? Dolor perferendis ullam impedit dolore hic qui
+					quisquam? Consectetur libero, similique optio neque modi
+					ipsa cumque eius dolores veritatis ducimus provident nisi
+					placeat dolor! Enim hic odio nam maxime architecto atque
+					blanditiis dolore molestias obcaecati aspernatur
+					reprehenderit omnis unde explicabo incidunt assumenda
+					provident debitis reiciendis quidem ea inventore quibusdam,
+					earum sapiente fuga dicta. Cumque quidem laborum, quo
+					voluptas debitis nemo adipisci placeat voluptates assumenda
+					deserunt, et unde minus at non atque! Sint rem repellat
+					nihil ullam quidem. Rerum esse ratione in animi ad unde qui
+					et tempore deserunt. Quaerat eveniet distinctio quos, vitae
+					minus ad omnis? Enim odit sit labore tenetur officiis nisi
+					placeat fugiat, animi, ut ipsa laboriosam quam tempora esse
+					nostrum sapiente assumenda! Corrupti consequatur facilis
+					perspiciatis, excepturi impedit nesciunt quaerat commodi
+					aperiam, porro nihil eum obcaecati accusantium quasi hic nam
+					cum tempora delectus ducimus inventore. Harum consequatur
+					porro eum vero debitis ad obcaecati suscipit neque officiis
+					vel. Voluptas, aperiam. Sequi beatae nostrum omnis,
+					veritatis labore eveniet minus tempora voluptates, ratione
+					voluptatibus consectetur libero maxime inventore blanditiis
+					non, sed atque voluptatum sit necessitatibus corporis alias
+					sunt aliquid exercitationem? Voluptatum commodi iure
+					accusamus totam, labore deleniti id incidunt culpa dolorum
+					voluptatem dicta voluptas fugit sed quibusdam! Sapiente
+					voluptatum possimus aliquid a nisi, asperiores distinctio
+					quis consequatur nulla deserunt debitis, odit, nemo nihil ex
+					placeat voluptatem repellendus ipsum inventore hic sint
+					harum ut totam. Laudantium atque saepe porro debitis
+					voluptatum ad labore, consectetur ipsum quod facilis,
+					tenetur nulla id sint eius? Eum repellendus nam mollitia
+					officia deleniti temporibus alias quis magni illo iste
+					dolores expedita doloribus perspiciatis earum esse eius
+					sunt, ea, sapiente non? Aut doloribus doloremque dolorem,
+					placeat fugit quisquam consequatur enim iusto maxime? Quo
+					ipsam veniam distinctio odio quis animi aliquid dolores at
+					quaerat sapiente deserunt eius commodi modi eum beatae,
+					consequatur cum facilis. Deserunt, accusantium libero aut
+					quia perferendis minus. Debitis pariatur consequuntur sunt
+					accusantium veniam vel. Nostrum, itaque? Eos exercitationem
+					eius minima atque! Rerum nihil et exercitationem, vel
+					explicabo repellat odio?
+				</div>
+			</div>
+		);
+	}
+}
